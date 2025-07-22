7dc16231f0a24a6ac2395195b2d9ee64bd3c5c97
aryan1429 <aryanaligeti@gmail.com>
2025-07-22 15:24:37 +0530

    added output doc and some minor changes

--- mlp_cifar.py
+++ mlp_cifar.py
@@ -2,6 +2,7 @@ from keras.models import Sequential
 from keras.layers import Dense, Flatten
 from keras.datasets import cifar10
 from keras.utils import to_categorical
+import matplotlib.pyplot as plt
 
 #import data
 (X_train, y_train),(X_test,y_test) = cifar10.load_data()
@@ -14,16 +15,21 @@ y_test = to_categorical(y_test)
 
 model = Sequential()
 model.add(Flatten(input_shape=(32,32,3)))
+model.add(Dense(256,activation = 'relu'))
 model.add(Dense(128,activation = 'relu'))
 model.add(Dense(10,activation = 'softmax'))
 
 #compile 
-model.compile(optimizer= 'Adam', loss = 'categorical_crossentropy')
+model.compile(optimizer= 'adam', loss = 'categorical_crossentropy',metrics = ['accuracy'])
 
 #train
 model.fit(X_train, y_train, epochs = 10, batch_size=64)
 
 #Evaluate
 model.evaluate(X_test,y_test)
+
+#Visualization
+
+plt.plot(epochs,)
 
 
