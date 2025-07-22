d4c6c8bd12b37f653afae3ac9ac47a407749bd9c
aryan1429 <aryanaligeti@gmail.com>
2025-07-22 16:01:05 +0530

    finishing touches for week3

--- mlp_cifar.py
+++ mlp_cifar.py
@@ -15,6 +15,8 @@ y_test = to_categorical(y_test)
 
 model = Sequential()
 model.add(Flatten(input_shape=(32,32,3)))
+model.add(Dense(1024,activation = 'relu'))
+model.add(Dense(512,activation = 'relu'))
 model.add(Dense(256,activation = 'relu'))
 model.add(Dense(128,activation = 'relu'))
 model.add(Dense(10,activation = 'softmax'))
@@ -23,13 +25,23 @@ model.add(Dense(10,activation = 'softmax'))
 model.compile(optimizer= 'adam', loss = 'categorical_crossentropy',metrics = ['accuracy'])
 
 #train
-model.fit(X_train, y_train, epochs = 10, batch_size=64)
+history = model.fit(X_train, y_train, epochs = 30, batch_size=64,validation_split= 0.5)
+#print(type(history))
+#print(history.history.keys())
+#print(history.history.items())
+
 
 #Evaluate
-model.evaluate(X_test,y_test)
+test_accuracy,loss = model.evaluate(X_test,y_test)
+print(f'test_accuracy : {test_accuracy}')
+print(f'loss: {loss}')
 
 #Visualization
 
-plt.plot(epochs,)
+plt.plot(history.history['accuracy'],color = 'blue',label = 'accuracy')
+plt.plot(history.history['val_accuracy'],color = 'red',label = 'val_accuracy')
+plt.legend()
+plt.title("Epochs vs Accuracy")
+plt.show()
 
 
