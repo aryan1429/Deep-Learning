676896cb17831789e8b35dd382bac948fa62a958
aryan1429 <aryanaligeti@gmail.com>
2025-07-22 14:18:47 +0530

    added upto flatten layer

---
+++ mlp_cifar.py
@@ -0,0 +1,16 @@
+from keras.models import Sequential
+from keras.layers import Dense, Flatten
+from keras.datasets import cifar10
+from keras.utils import to_categorical
+
+#import data
+(X_train, y_train),(X_test,y_test) = cifar10.load_data()
+
+#categorical 
+y_train = to_categorical(y_train)
+y_test = to_categorical(y_test)
+
+#build the Architecture
+
+model = Sequential()
+model.add(Flatten(input_shape=(32,32,3)))
